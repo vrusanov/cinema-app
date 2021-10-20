4ec058c8075604f755a9c9188a970ab6c40036a8
vrusanov <vasiliy.rusanov@gmail.com>
2021-10-20 11:24:49 +0300

    first commit

---
+++ components/HeaderPart.vue
@@ -0,0 +1,86 @@
+<template>
+<div class="main-container">
+  <img src="../assets/img/background-cinema-app.jpg" alt="background-img">
+  <div class="text-container">
+    <div class="text">
+      <span class="mini-title">Search Now</span>
+      <h1><span>Booking</span>Now</h1>
+      <a href="#cinema-grid" class="button">View cinema</a>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "MainPage"
+}
+</script>
+
+<style lang="scss" scoped>
+.main-container {
+  height: 400px;
+  position: relative;
+  @media (min-width: 750px) {
+    height: 500px;
+  }
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background-color: rgba(0, 0, 0, 0.6);
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  .text-container {
+    z-index: 99;
+    position: absolute;
+    top: 0;
+    margin: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    .text {
+      padding: 0 16px;
+      width: 100%;
+      max-width: 1400px;
+      margin: 0 auto;
+    }
+    .mini-title {
+      font-weight: 600;
+      font-size: 18px;
+      text-transform: uppercase;
+      color: #c92502;
+      margin-bottom: 8px;
+      @media (min-width: 750px) {
+        font-size: 22px;
+      }
+    }
+    h1 {
+      color: #fff;
+      font-size: 64px;
+      font-weight: 200;
+      margin-bottom: 8px;
+      @media (min-width: 750px) {
+        font-size: 84px;
+      }
+      span {
+        font-weight: 500;
+      }
+    }
+    a.button {
+      font-size: 20px;
+      color: #fff;
+      align-self: flex-start;
+    }
+  }
+}
+</style>
---
+++ pages/cinemas/_cinemaid.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "_cinemaid"
+}
+</script>
+
+<style scoped>
+
+</style>
